60dab1acea71b868c13021bf83bdbf8204ef6c37
Adityan Nampoothiry <165512407+adtyn-0@users.noreply.github.com>
2025-12-19 13:10:42 +0530

    64232

--- orders/models.py
+++ orders/models.py
@@ -8,3 +8,12 @@ class OrderStatus(models.Model):
     order_status = models.CharField(max_length=50,unique=True)
     def __str__(self):
         return self.order_status
+
+class Order(models.Model):
+    """
+    Represents a customer order, linked with their status
+    """
+    status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True,blank=True)
+
+    def __str__(self):
+        return f"Order {self.pk} - {self.status}

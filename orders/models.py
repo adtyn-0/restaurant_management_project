c33f145056cc3a9a21cda06fb1eda76a67448b49
Adityan Nampoothiry <165512407+adtyn-0@users.noreply.github.com>
2025-12-19 13:00:38 +0530

    64233

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class OrderStatus(models.Model):
+    """
+    Represents the state of order in workflow with states like: Pending, Completed or Cancelled etc. 
+    """
+    order_status = models.CharField(max_length=50,unique=True)
+    def __str__(self):
+        return self.order_status

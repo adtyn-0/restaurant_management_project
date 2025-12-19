08ff5d47651860c0eb084f64814dd68258fb2b69
Adityan Nampoothiry <165512407+adtyn-0@users.noreply.github.com>
2025-12-19 13:27:36 +0530

    64235

--- home/views.py
+++ home/views.py
@@ -4,4 +4,9 @@ from .models import MenuCategory
 from .serializers import MenuCategorySerializer
 
 # Create your views here.
-
+class MenuCategoryListView(ListAPIView):
+    """
+    returns a list of all available menu categories
+    """
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer

1890d8cd8e4ea80cf8ae2e93c06b6acfb3617579
Adityan Nampoothiry <165512407+adtyn-0@users.noreply.github.com>
2025-12-19 13:22:44 +0530

    64235

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
 
 # Create your views here.
 
